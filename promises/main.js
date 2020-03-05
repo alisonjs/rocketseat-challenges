16ca6684a1ce73715534a5d7c56dfad3505dc1cc
Alison <alison.sousa@info.ufrn.br>
2020-03-05 13:59:15 -0300

    Promisse Challenge

--- promises/main.js
+++ promises/main.js
@@ -1,7 +1,56 @@
-axios.get()
+const inputUserElement = document.querySelector('#user');
+const buttonUserElement = document.querySelector('#search-repos');
+const listReposElement = document.querySelector('#repos ul');
+
+function searchRepos(){
+    const username = inputUserElement.value;
+    const url = 'https://api.github.com/users/'+username+'/repos';
+    inputUserElement.value = '';
+    axios.get(url)
+    .then(function (response){
+        listReposElement.innerHTML = '';
+        const repos = response.data;
+        for(repo of repos){
+            const textElement = document.createTextNode(repo.name);
+            const itemElement = document.createElement('li');
+            itemElement.appendChild(textElement);
+            listReposElement.appendChild(itemElement);
+        }
+    })
+    .catch(function (error){
+        console.log(error);
+    });
+}
+
+
+
+
+
+/*
+axios.get('http://api.github.com/users/diego3g')
 .then(function(response){
     console.log(response);
 })
 .catch(function(error){
     console.log(error);
 });
+
+
+function checaIdade(idade){
+    return new Promise(function(resolve, reject){
+        if(idade > 18){
+            resolve('Maior que 18.');
+        }else{
+            reject('Menor que 18.');
+        }
+    });
+}
+
+checaIdade(18)
+.then(function(response){
+    console.log(response);
+})
+.catch(function(error){
+    console.log(error);
+});
+*/
